0cdec79657aa85d5bb9b14c2f234572a469dd20e
Nicolas Ardison <nicolas.ard@gmail.com>
2021-06-24 00:16:53 -0300

    - Fix minor finding

--- src/main/java/io/github/unterstein/entity/OrderBookLine.java
+++ src/main/java/io/github/unterstein/entity/OrderBookLine.java
@@ -1,6 +1,22 @@
 package io.github.unterstein.entity;
 
 public class OrderBookLine {
-    double price;
-    double quantity;
+    private double price;
+    private double quantity;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
 }
